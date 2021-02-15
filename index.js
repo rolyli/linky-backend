a05f11516b607fe9fec4dd14e68cefc2025e2638
rolyli <leem72104@gmail.com>
2021-02-15 15:07:38 +0000

    Remove testing code from index.js

--- index.js
+++ index.js
@@ -3,24 +3,21 @@ const cors = require("cors");
 const app = express();
 const router = express.Router();
 
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const postController = require("./controllers/postController");
 
-app.post('/submit-form', (req, res) => {
-  const username = req.body.username
-  console.log(req.body)
-  res.end()
-})
-
-
 app.use(cors());
 app.use(express.static("build"));
+
 router.use("/posts/:page?", postController.post_list);
 router.use("/post/:id", postController.post_detail);
 router.post("/post", postController.post_post);
 router.get("/post", postController.post_detail_list);
+
 app.use("/api", router);
 app.listen(process.env.PORT || 3001);
